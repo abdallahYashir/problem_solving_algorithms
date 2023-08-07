8fe523dbbc50ee3f9148548dda7795dfddde0970
Abdallah Yashir Ramsing <yashircr@gmail.com>
2023-08-07 07:59:16 +0000

    Check if k is over 26

--- algorithms/caesar_cipher.rb
+++ algorithms/caesar_cipher.rb
@@ -1,6 +1,7 @@
 def caesarCipher(s, k)
     s.split("").map { |letter|
         if letter =~ /[a-zA-Z]/
+            k = k % 26 if k > 26
             letter = letter.ord + k
             if letter > 122 || (letter > 90 && letter < 97)
                 letter -= 26
